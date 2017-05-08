4f38b2d047756029a380ad19c99e581ed9b629c5
Caroline Kettlestrings <caroline.kettlestrings@here.com>
2017-05-07 21:58:50 -0500

    Rename diverging implicits stuff

---
+++ src/test/scala/com/sandbox/divergingimplicits/DivergingImplicitsDueToInfiniteExpansion.scala
@@ -0,0 +1,19 @@
+package com.sandbox.divergingimplicits
+
+import org.scalatest.FlatSpec
+
+/** Uncommenting the method call in this class will cause the failure described in the comments */
+class DivergingImplicitsDueToInfiniteExpansion extends FlatSpec {
+  implicit def fooOne[T](implicit a: Set[T]): T = a.head
+
+  def fooTwo(implicit x: Set[Int]): Unit = {}
+
+  // The compiler sees that fooTwo needs an implicit Set[Int]. It sees that fooOne[Set[Int]]
+  // can provide it, but needs an implicit Set[Set[Int]]. It then sees that fooOne[Set[Set[Int]]]
+  // can provide a Set[Set[Int]], but needs an implicit Set[Set[Set[Int]]]. It gives up because
+  // this can clearly go on forever.
+  "fooTwo" should "fail with 'diverging implicit expansion for type Set[Int] starting with method fooOne' " in {
+    assertDoesNotCompile("fooTwo")
+    //  fooTwo
+  }
+}
